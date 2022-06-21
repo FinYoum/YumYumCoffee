03864843ccc9153d15a73906863a9435a8f029f1
FinYoum <hwyoum@knou.ac.kr>
2022-06-21 19:04:03 +0900

    20220621 오라클 db연동

--- Yumyum/src/main/java/com/yum/YumyumApplication.java
+++ Yumyum/src/main/java/com/yum/YumyumApplication.java
@@ -1,4 +1,4 @@
-package com.Yum;
+package com.yum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Yumyum/src/test/java/com/yum/YumyumApplicationTests.java
+++ Yumyum/src/test/java/com/yum/YumyumApplicationTests.java
@@ -1,4 +1,4 @@
-package com.Yum;
+package com.yum;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
