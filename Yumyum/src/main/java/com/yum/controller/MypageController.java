db307f8505125b75208313cfd740ff5fd277d544
FinYoum <hwyoum@knou.ac.kr>
2022-07-22 21:38:10 +0900

    AOP 로그인 체크 httpSession사용하는 메소드에서 로그인 체크 작동 확인

--- Yumyum/src/main/java/com/yum/controller/MypageController.java
+++ Yumyum/src/main/java/com/yum/controller/MypageController.java
@@ -1,11 +1,15 @@
 package com.yum.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.yum.constant.SessionConstants;
 import com.yum.domain.CouponDTO;
 import com.yum.domain.MemberDTO;
 import com.yum.service.MypageService;
@@ -19,13 +23,11 @@ public class MypageController {
 	
 //	마이페이지 회원정보 불러오기
 	@GetMapping(value="/mypage")
-	public String openMypage(Model model) {
-		int userNum=1; //회원번호
-		MemberDTO member = mypageService.getUserDetail(userNum);
+	public String openMypage(Model model, HttpSession session) {
+		MemberDTO member = (MemberDTO)session.getAttribute(SessionConstants.loginMember);
 		model.addAttribute("member",member);
-		
 		CouponDTO params = new CouponDTO();
-		params.setUserNum(userNum);
+		params.setUserNum(member.getUserNum());
 		int couponTotalCount = mypageService.countCoupon(params);
 		model.addAttribute("couponTotalCount",couponTotalCount);
 		return "mypage/mypage";
@@ -33,20 +35,16 @@ public class MypageController {
 		
 //	마이페이지 >> 과거 주문 내역 페이지
 	@GetMapping(value="/orderhistory")
-	public String orderhistory(
-				@RequestParam(value = "userNum", required = true) int userNum
-								, Model model) {
-		MemberDTO member = mypageService.getUserDetail(userNum);
+	public String orderhistory(Model model, HttpSession session) {
+		MemberDTO member = (MemberDTO)session.getAttribute(SessionConstants.loginMember);
 		model.addAttribute("member", member);		
 		return "mypage/orderHistory";
 	}
 	
 //	마이페이지 >> 내 정보 수정
 	@GetMapping(value="/updateuser")
-	public String updateuser(
-				@RequestParam(value = "userNum", required = true) int userNum
-								, Model model) {
-		MemberDTO member = mypageService.getUserDetail(userNum);
+	public String updateuser(Model model, HttpSession session) {
+		MemberDTO member = (MemberDTO)session.getAttribute(SessionConstants.loginMember);;
 		model.addAttribute("member", member);		
 		return "mypage/updateUser";
 	}
