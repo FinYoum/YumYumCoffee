dba849c6ee6b7442b574891e378e0fb58610e46b
FinYoum <hwyoum@knou.ac.kr>
2022-07-28 12:23:59 +0900

    실행이 안되서 파일 수정해서 재업

--- Yumyum/src/main/java/com/yum/controller/LoginController.java
+++ Yumyum/src/main/java/com/yum/controller/LoginController.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,6 +23,7 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 import com.yum.constant.SessionConstants;
 import com.yum.domain.MemberDTO;
 import com.yum.service.EmailService;
+//import com.yum.service.EmailService;
 import com.yum.service.MemberService;
 
 
@@ -35,8 +35,6 @@ public class LoginController {
 	private MemberService memberService;
 	@Autowired
     private PasswordEncoder passwordEncoder;
-	@Autowired
-    private AuthenticationManager authenticationManager; // 얘를 통해 세션값 변경해야함
 	@Autowired
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 	
