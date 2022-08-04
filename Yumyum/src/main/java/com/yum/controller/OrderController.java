ee7b54aa8fae15e3df436b6db4378f12699bcbd7
FinYoum <hwyoum@knou.ac.kr>
2022-08-04 18:32:59 +0900

    장바구니 데이터 >OrderController에서 가져오기 완료

--- Yumyum/src/main/java/com/yum/controller/OrderController.java
+++ Yumyum/src/main/java/com/yum/controller/OrderController.java
@@ -1,27 +1,24 @@
 package com.yum.controller;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonArray;
 import com.yum.constant.SessionConstants;
 import com.yum.domain.CartDTO;
 import com.yum.domain.CouponDTO;
@@ -30,7 +27,6 @@ import com.yum.domain.OrderDTO;
 import com.yum.service.MypageService;
 import com.yum.service.PaymentService;
 
-import groovy.transform.Undefined.EXCEPTION;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -106,31 +102,31 @@ public class OrderController {
 	//"cartList" :JSON.stringify(totalArr)
 //	@RequestParam(value="cartList[]") List <CartDTO> cartList, 
 //	   @RequestParam int cnt, @RequestParam int totalPrice,
-		@ResponseBody
-	   @RequestMapping(value = {"/order"}, method = { RequestMethod.POST, RequestMethod.PATCH }    )
+//		@ResponseBody
+	   @PostMapping(value = {"/order"})
+//	   @RequestMapping(value = {"/order"}, method = { RequestMethod.POST, RequestMethod.PATCH }    )
 		public String openOrderList( @RequestParam  Map<String, Object> param,
-			   HttpSession session, Model model) throws JsonMappingException, JsonProcessingException {      
+			   HttpSession session, Model model) throws JsonMappingException, JsonProcessingException {
+		   
+		   		MemberDTO  member=(MemberDTO)session.getAttribute(SessionConstants.loginMember);
+		   
 				log.info("cartList");
-				//log.info("cartList : {}, {} , {}", cnt , totalSum , param.size());	
 				log.info("cartList : {}", param.toString());
+				String test= param.get("cartList").toString();
+				log.info(test);
+				int cntProduct=Integer.parseInt(param.get("cnt").toString());
+				int totalSum=Integer.parseInt(param.get("totalSum").toString());
 				
-				String cnt=(String)param.get("cnt");
-				String totalSum=(String)param.get("totalSum");
-		
+				
 				ObjectMapper objectMapper=new ObjectMapper();
-				List<CartDTO>  orderList=objectMapper.readValue(param.get("cartList").toString(), 
+				List<CartDTO>  cartList=objectMapper.readValue(param.get("cartList").toString(), 
 						new TypeReference<List<CartDTO>>(){});
 				
-				//Object object=(Object)param.get("cartList");
-//				List<CartDTO> cartList=(List<CartDTO>)param.get("cartList");
-				log.info("cartList : {}",cnt);
-				log.info("cartList : {}",totalSum);
-				log.info("cartList : {}",orderList.size());
 				
-				for(CartDTO cartDTO :  orderList ) {
-					log.info("주문 목록 {}" , cartDTO.toString());
-					//DB 저장
-				}
+				model.addAttribute("totalPrice",totalSum);
+				model.addAttribute("totalQty",cntProduct);
+				model.addAttribute("cartList", cartList);
+				
 				
 				
 	      //주문리스트와 이미지 테이블을 조인해서 모델에 저장
